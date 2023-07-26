f231fad919e13721b1d94b5048bbabc3307bd6ce
huynhthien2709 <huynhthien2709@gmail.com>
2023-07-26 22:35:05 +0700

    add, update, delete customer

--- src/main/java/com/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/springdemo/rest/CustomerRestController.java
@@ -3,8 +3,12 @@ package com.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +44,45 @@ public class CustomerRestController {
 		}
 		return customer;
 	}
+	
+	//Lab 17.7 
+	//add mapping for POST customers - add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		//also just in case the pass and id in JSON ... set id to 0
+		//this is force a save of item ...instead of update
+		customer.setId(0);
+		customerService.saveCustomer(customer);
+		return customer;
+	}
+	
+	//Lab 17.8
+	//add mapping for PUT customers - update existing customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
+		
+		customerService.saveCustomer(customer);
+		
+		return customer;
+	}
+	
+	//Lab 17.9
+	//add mapping for DELETE /customers/{customerId} - delete customer
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId ) {
+		
+		Customer customer = customerService.getCustomer(customerId);
+		if (customer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+		customerService.deleteCustomer(customerId); 
+		return "Delete customer id - " + customerId;
+	}
+	
+	
+	
+	
+	
+	
 	
 }
